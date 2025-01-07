3fbd124c0429b8095ca1565bc921417178087906
ashut2003 <147096869+ashut2003@users.noreply.github.com>
2025-01-07 17:25:08 +0530

    landingpage-Support

---
+++ CreateTicket.js
@@ -0,0 +1,112 @@
+import React from 'react'
+function CreateTicket () {
+    return ( 
+        <div className='container'>
+            <div className='row p-5 mt-2 mb-5'>
+                <h1 className='fs-2 '>
+                   To create a ticket, select a relevant topic
+                </h1>
+                <div className='col-4 p-5 mt-2 '>
+                <h4 className=' '>
+                <i class="fa fa-plus-circle " aria-hidden="true"></i>Account Opening
+                </h4>
+                
+                   <a href=" " style={{textDecoration:"none", lineHeight:"2.5"}}> Offline Account OpeningCompany</a><br/>
+                   <a href=" " style={{textDecoration:"none", lineHeight:"2.5"}}> Online Account Opening</a><br/>
+                   <a href=" " style={{textDecoration:"none", lineHeight:"2.5"}}> Partnership and HUF Account</a><br/>
+                   <a href=" " style={{textDecoration:"none", lineHeight:"2.5"}}> Opening</a><br/>
+                   <a href=" " style={{textDecoration:"none", lineHeight:"2.5"}}> NRI Account Opening</a><br/>
+                   <a href=" " style={{textDecoration:"none", lineHeight:"2.5"}}> Charges at Zerodha</a><br/>
+                   <a href=" " style={{textDecoration:"none", lineHeight:"2.5"}}> Zerodha IDFC FIRST Bank 3-in-1 Account</a><br/>
+                   <a href=" " style={{textDecoration:"none", lineHeight:"2.5"}}> getting Started</a>
+
+                
+            </div>
+            <div className='col-4 p-5 mt-2 '>
+                <h4 className=' '>
+                <i class="fa fa-user-circle-o" aria-hidden="true"></i>Your Zerodha Account
+                </h4>
+                
+                   <a href=" " style={{textDecoration:"none", lineHeight:"2.5"}}> Login Credential</a><br/>
+                   <a href=" " style={{textDecoration:"none", lineHeight:"2.5"}}> Account modification and Segment Addition</a><br/>
+                   <a href=" " style={{textDecoration:"none", lineHeight:"2.5"}}> DP ID and Bank Details</a><br/>
+                   <a href=" " style={{textDecoration:"none", lineHeight:"2.5"}}> Your Profile</a><br/>
+                   <a href=" " style={{textDecoration:"none", lineHeight:"2.5"}}> Transfer and conversion of shares</a>
+                   
+
+                
+            </div>
+            <div className='col-4 p-5 mt-2 '>
+                <h4 className=' '>
+                <i class="fa fa-bar-chart" aria-hidden="true"></i>Your Zerodha Account
+                </h4>
+                
+                   <a href=" " style={{textDecoration:"none", lineHeight:"2.5"}}> Margin/leverage, Product and Order types</a><br/>
+                   <a href=" " style={{textDecoration:"none", lineHeight:"2.5"}}> Kite web and Mobile</a><br/>
+                   <a href=" " style={{textDecoration:"none", lineHeight:"2.5"}}> Trading FAQs</a><br/>
+                   <a href=" " style={{textDecoration:"none", lineHeight:"2.5"}}> OCorporate Actions</a><br/>
+                   <a href=" " style={{textDecoration:"none", lineHeight:"2.5"}}> Sentinel</a><br/>
+                   <a href=" " style={{textDecoration:"none", lineHeight:"2.5"}}> Kite API</a><br/>
+                   <a href=" " style={{textDecoration:"none", lineHeight:"2.5"}}> PI and Other platform</a><br/>
+                   <a href=" " style={{textDecoration:"none", lineHeight:"2.5"}}> Stockeports</a><br/>
+                   <a href=" " style={{textDecoration:"none", lineHeight:"2.5"}}> GTT</a>
+
+                
+            </div>
+
+            <div className='col-4 p-5 mt-2 '>
+                <h4 className=' '>
+                <i class="fa fa-credit-card" aria-hidden="true"></i>Funds
+                </h4>
+                
+                   <a href=" " style={{textDecoration:"none", lineHeight:"2.5"}}> Offline Account OpeningCompany</a><br/>
+                   <a href=" " style={{textDecoration:"none", lineHeight:"2.5"}}> Online Account Opening</a><br/>
+                   <a href=" " style={{textDecoration:"none", lineHeight:"2.5"}}> Partnership and HUF Account</a><br/>
+                   <a href=" " style={{textDecoration:"none", lineHeight:"2.5"}}> Opening</a><br/>
+                   <a href=" " style={{textDecoration:"none", lineHeight:"2.5"}}> NRI Account Opening</a><br/>
+                   <a href=" " style={{textDecoration:"none", lineHeight:"2.5"}}> Charges at Zerodha</a><br/>
+                   <a href=" " style={{textDecoration:"none", lineHeight:"2.5"}}> Zerodha IDFC FIRST Bank 3-in-1 Account</a><br/>
+                   <a href=" " style={{textDecoration:"none", lineHeight:"2.5"}}> getting Started</a>
+
+                
+            </div>
+            <div className='col-4 p-5 mt-2 '>
+                <h4 className=' '>
+                <i class="fa fa-circle-o-notch" aria-hidden="true"></i>Console
+                </h4>
+                
+                   <a href=" " style={{textDecoration:"none", lineHeight:"2.5"}}> Login Credential</a><br/>
+                   <a href=" " style={{textDecoration:"none", lineHeight:"2.5"}}> Account modification and Segment Addition</a><br/>
+                   <a href=" " style={{textDecoration:"none", lineHeight:"2.5"}}> DP ID and Bank Details</a><br/>
+                   <a href=" " style={{textDecoration:"none", lineHeight:"2.5"}}> Your Profile</a><br/>
+                   <a href=" " style={{textDecoration:"none", lineHeight:"2.5"}}> Transfer and conversion of shares</a>
+                   
+
+                
+            </div>
+            <div className='col-4 p-5 mt-2 '>
+                <h4 className=' '>
+                <i class="fa fa-circle-o" aria-hidden="true"></i>coins
+                </h4>
+                
+                   <a href=" " style={{textDecoration:"none", lineHeight:"2.5"}}> Margin/leverage, Product and Order types</a><br/>
+                   <a href=" " style={{textDecoration:"none", lineHeight:"2.5"}}> Kite web and Mobile</a><br/>
+                   <a href=" " style={{textDecoration:"none", lineHeight:"2.5"}}> Trading FAQs</a><br/>
+                   <a href=" " style={{textDecoration:"none", lineHeight:"2.5"}}> OCorporate Actions</a><br/>
+                   <a href=" " style={{textDecoration:"none", lineHeight:"2.5"}}> Sentinel</a><br/>
+                   <a href=" " style={{textDecoration:"none", lineHeight:"2.5"}}> Kite API</a><br/>
+                   <a href=" " style={{textDecoration:"none", lineHeight:"2.5"}}> PI and Other platform</a><br/>
+                   <a href=" " style={{textDecoration:"none", lineHeight:"2.5"}}> Stockeports</a><br/>
+                   <a href=" " style={{textDecoration:"none", lineHeight:"2.5"}}> GTT</a>
+
+                
+            </div>
+            </div>
+            
+            </div>
+            
+
+     );
+}
+
+export default CreateTicket ;
--- Hero.js
+++ Hero.js
@@ -1,18 +1,39 @@
 import React from 'react'
 function Hero() {
     return ( 
-       <div className='container border-bottom mb-5 '>
-        <div className='text-center'>
-        <h1>Technology</h1>
-        <h3 className='text-muted mt-3 fs-4'>Sleek, modern and intuitive trading platform</h3>
-        <p className='mt-3 '>check out our {" "}
-            <a href="" style={{textDecoration:"none"}}> investment Offering{" "}
-            <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
-            </a>
-        </p>
-       </div>
-       </div>
+        //bootstrap ka full use hai class or id bannae me
+        <section className='container-fluid ' id="supportHero">
+        <div className=' p-5 ' id="supportWrapper">
+           <h4>Support Portal
+
+           </h4>
+            <a href=" " style={{textDecoration:"none" ,fontSize:"20px"}}>Track Ticket</a>
+            </div>
+
+            <div className='row p-5 m-3' >
+            <div className='col-6 p-5 ' >
+                <h1 className='fs-3'>Search  for an Answer or browser help topics to create a ticket</h1>
+                <input placeholder='eg. how do I activate F&O, why is my order getting rejected '/>
+                <br/>
+               <a href=" " >Track Account Opening</a>
+               <a href=" " >Track Segment Activation</a>
+               <a href=" " >Intraday margin</a>
+               <a href=" "> Kite user manual</a>
+                 </div>
+            <div className='col-6 p-5 ' > 
+                <h1 className='fs-3'>Featured</h1>
+                <ol>
+                    <li><a href=" " >Current Takeover and Delisting january 2024</a></li>
+                    <li><a href=" " >Latest Intraday leverage  MIS & CO</a></li>
+                </ol>
+                
+                
+            </div>
+          
+            
+            </div>
+            </section>
      );
 }
 
-export default Hero ;
+export default Hero;
